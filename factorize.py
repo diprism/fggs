02b833c0c52ab978f88bd937c4e0c522e1a62b5c
David Chiang <dchiang@nd.edu>
2021-05-28 16:42:38 -0400

    added another exact treewidth method

--- factorize.py
+++ factorize.py
@@ -7,7 +7,7 @@ def add_node(graph, v):
         graph[v] = set()
 
 def remove_node(graph, v):
-    for u in graph:
+    for u in graph[v]:
         graph[u].discard(v)
     del graph[v]
 
@@ -30,11 +30,11 @@ def make_clique(graph, nodes):
             if v1 != v2:
                 add_edge(graph, v1, v2)
 
-def count_fillin(graph, nodes):
+def count_fillin(graph, u):
     """How many edges would be needed to make v a clique."""
     count = 0
-    for v1 in nodes:
-        for v2 in nodes:
+    for v1 in graph[u]:
+        for v2 in graph[u]:
             if v1 != v2 and v2 not in graph[v1]:
                 count += 1
     return count//2
@@ -51,7 +51,7 @@ def simplicial(graph, v):
 
 def almost_simplicial(graph, v):
     for u in graph[v]:
-        if is_clique(graph, set(graph[v]) - {u}):
+        if is_clique(graph, graph[v] - {u}):
             return True
     return False
 
@@ -61,48 +61,46 @@ def eliminate_node(graph, v):
     make_clique(graph, graph[v])
     remove_node(graph, v)
 
-def upper_bound(graph):
-    """Min-fill."""
+def min_fill(graph):
+    """Min-fill upper-bound."""
     graph = copy_graph(graph)
     dmax = 0
     order = []
     while len(graph) > 0:
-        #d, u = min((len(graph[u]), u) for u in graph) # min-width
-        u = min(graph, key=lambda u: count_fillin(graph, graph[u]))
+        u = min(graph, key=lambda u: count_fillin(graph, u))
         dmax = max(dmax, len(graph[u]))
         eliminate_node(graph, u)
         order.append(u)
     return dmax, order
 
-def lower_bound(graph):
-    """Minor-min-width"""
+def minor_min_width(graph):
+    """Minor-min-width lower-bound."""
     graph = copy_graph(graph)
     dmax = 0
     while len(graph) > 0:
-        # pick node of minimum degree
-        u = min(graph, key=lambda u: len(graph[u]))
-        dmax = max(dmax, len(graph[u]))
+        # Let v be a node of minimum degree
+        v = min(graph, key=lambda v: len(graph[v]))
+        dmax = max(dmax, len(graph[v]))
 
-        # Gogate and Dechter: minor-min-width
-        nb = set(graph[u]) - {u}
+        nb = graph[v]
+        assert v not in nb
         if len(nb) > 0:
-            v = min(nb, key=lambda u: len(set(graph[u]) & nb))
+            # Let u \in N(v) such that the degree of u is minimum in N(v)
+            #u = min(nb, key=lambda u: len(graph[u])) # min-d (this is what Gogate's code does)
+            u = min(nb, key=lambda u: len(graph[u] & nb)) # least-c (Bodlaender and Koster say this works better)
             contract_edge(graph, u, v)
         else:
-            remove_node(graph, u)
+            # v has degree zero, so the rest will too
+            break
     return dmax
 
-class Bag:
-    def __init__(self, nodes):
-        self.nodes = set(nodes)
-    def __str__(self):
-        return str(self.nodes)
-    def __repr__(self):
-        return f'Bag({self.nodes})'
+upper_bound = min_fill
+lower_bound = minor_min_width
 
 def quickbb(graph):
     """Gogate and Dechter, A complete anytime algorithm for treewidth. UAI
     2004. http://arxiv.org/pdf/1207.4109.pdf
+    Code: https://github.com/dechterlab/quickbb
 
     Given a permutation of the nodes (called an elimination ordering),
     for each node, remove the node and make its neighbors into a clique.
@@ -131,11 +129,12 @@ def quickbb(graph):
 
     best_ub = best_order = None
     
-    def bb(graph, order, f, g):
+    def bb(graph, order, sep, f, g):
         """Branch-and-bound search.
 
         graph: graph with some nodes eliminated
         order: the eliminated nodes (in order)
+        sep: neighbors of order[-1]
         f: lower bound on treewidth so far
         g: treewidth so far
         """
@@ -154,12 +153,15 @@ def quickbb(graph):
                     almost_simplicial(graph, v) and len(graph[v]) <= lb):
                     vs = [v]
                     break
-                else:
+                elif v not in sep: # Section 5.2
                     vs.append(v)
 
+            # to do: sort by min-fill?
+            #vs.sort(key=lambda u: count_fillin(graph, graph[u]))
+            
             for v in vs:
                 graph1 = copy_graph(graph)
-                exclude1 = set(graph[v])
+                sep1 = graph[v]
                 eliminate_node(graph1, v)
                 order1 = order + [v]
                 # treewidth for current order so far
@@ -167,15 +169,136 @@ def quickbb(graph):
                 # lower bound given where we are
                 f1 = max(g, lower_bound(graph1)) 
                 if f1 < best_ub:
-                    bb(graph1, order1, f1, g1)
+                    bb(graph1, order1, sep1, f1, g1)
 
     order = []
     best_ub, best_order = upper_bound(graph)
     lb = lower_bound(graph)
     if lb < best_ub:
-        bb(graph, order, lb, 0)
+        bb(graph, order, [], lb, 0)
 
-    # Build the tree decomposition
+    return best_ub, best_order
+
+def connected_components(g, s=frozenset()):
+    """Find the connected components of g \ s."""
+
+    comps = []
+    nodes = set(g) - s
+    while len(nodes) > 0:
+        comp = set()
+        agenda = {nodes.pop()}
+        while len(agenda) > 0:
+            v = agenda.pop()
+            comp.add(v)
+            agenda.update(g[v] - comp - s)
+        nodes -= comp
+        comps.append(comp)
+    return comps
+
+def acb(g):
+    comptrees = []
+    for c in connected_components(g):
+        c = {v:g[v] for v in c}
+        ub, _ = upper_bound(c)
+        for k in range(1, ub+1):
+            comptree = acb_connected(c, k)
+            if comptree is not False:
+                comptrees.append(comptree)
+                break
+        else:
+            assert False
+    if len(comptrees) == 1:
+        tree = comptrees[0]
+    else:
+        tree = (frozenset(), comptrees)
+
+    # un-root the tree decomposition
+    unrooted = {}
+    def build(node):
+        bag, children = node
+        add_node(unrooted, bag)
+        for child in children:
+            add_edge(unrooted, bag, build(child))
+        return bag
+    build(tree)
+
+    return unrooted
+
+def acb_connected(g, k):
+    """
+    Complexity of finding embeddings in a $k$-tree.
+    Stefan Arnborg, Derek G. Corneil, and Andrzej Proskurowski.
+    https://epubs.siam.org/doi/abs/10.1137/0608024
+    
+    """
+    import itertools
+    
+    assert len(connected_components(g)) == 1
+    
+    if len(g) <= k+1:
+        return (set(g), [])
+
+    chart = {}
+    # for each set C_i of k vertices in G
+    for i in itertools.combinations(g, k):
+        i = frozenset(i)
+        # if C_i is a separator of G
+        comps = connected_components(g, i)
+        if len(comps) > 1:
+            chart[i] = {}
+            # C_i^j is (connected component of G \ C_i) | (complete graph on C_i)
+            for c in comps:
+                j = frozenset(c|i)
+                chart[i][j] = None
+    if len(chart) == 0:
+        return False
+
+    dead = set()
+    
+    # for each graph C_i^j in increasing order of size
+    bysize = sorted((len(j), i, j) for i in chart for j in chart[i])
+    for (h,i,j) in bysize:
+        # A graph of size k+1 (or less) is a partial k-tree
+        if h <= k+1:
+            chart[i][j] = (j, [])
+            continue
+        for v in j - i:
+            # examine all k-vertex separators C_m in C_i | {v},
+            # except C_i itself (not stated in pseudocode; see Lemma 4.4)
+            bag = i | {v}
+            children = []
+            union = set()
+            for u in i:
+                m = bag - {u}
+                # consider all C_m^l in C_i^j which are partial k-trees
+                if m in chart:
+                    for l in chart[m]:
+                        lm = l-m
+                        if lm.issubset(j-bag) and chart[m][l] not in [None, False]:
+                            # the C_m^l \ C_m are disjoint but there could be repeats
+                            if len(lm & union) == 0:
+                                union |= lm
+                                children.append(chart[m][l])
+                            else:
+                                assert lm.issubset(union)
+            # if the C_m^l \ C_m partition C_i^j - C_i - {v}, then set answer to YES
+            if union == j-bag:
+                chart[i][j] = (bag, children)
+                break
+
+        # if no answer was set for C_i^j, set answer to NO
+        if chart[i][j] is None:
+            chart[i][j] = False
+            dead.add(i)
+        # if each separator C_i has a C_i^j with answer NO, return NO
+        if len(dead) == len(chart):
+            return False
+        # if G has a separator C_i such that all C_i^j graph have YES, return YES
+        if all(chart[i][j] not in [None, False] for j in chart[i]):
+            return (i, [chart[i][j] for j in chart[i]])
+    assert False
+
+def tree_decomposition_from_order(graph, order):
     tree = {}
     def build(order):
         v = order[0]
@@ -184,20 +307,32 @@ def quickbb(graph):
         if len(clique) < len(graph):
             build(order[1:])
             for tv in tree:
-                if clique.issubset(tv.nodes):
+                if clique.issubset(tv):
                     break
             else:
                 assert False
         else:
             tv = None
-        tnew = Bag(clique|{v})
+        tnew = frozenset(clique|{v})
         add_node(tree, tnew)
         if tv is not None:
             add_edge(tree, tv, tnew)
-    build(best_order)
+    build(order)
     return tree
+    
+def tree_decomposition(graph, method='min_fill'):
+    if method == 'quickbb':
+        _, order = quickbb(graph)
+        return tree_decomposition_from_order(graph, order)
+    elif method == 'min_fill':
+        _, order = min_fill(graph)
+        return tree_decomposition_from_order(graph, order)
+    elif method == 'acb':
+        return acb(graph)
+    else:
+        raise ValueError("unknown method '{method}'")
 
-def factorize_rule(rule):
+def factorize_rule(rule, method='min_fill'):
     """Factorize a rule into one or more smaller rules, hopefully with
     lower maximum treewidth.
     """
@@ -213,16 +348,16 @@ def factorize_rule(rule):
                 if u != v:
                     g[u].add(v)
                     g[v].add(u)
-    t = quickbb(g)
+    t = tree_decomposition(g, method=method)
 
     # The root bag is the one that contains all the externals
     ext = set(rhs.ext())
     for bag in t:
-        if ext.issubset(bag.nodes):
+        if ext.issubset(bag):
             root = bag
             break
     else:
-        assert False
+        assert False, "There must be a bag containing all external nodes"
 
     newrules = []
     i = 0
@@ -235,7 +370,7 @@ def factorize_rule(rule):
             ext = rule.rhs().ext()
             lhs = rule.lhs()
         else:
-            ext = list(bag.nodes & parent.nodes)
+            ext = list(bag & parent)
             lhs = fggs.EdgeLabel(f'{rule.rule_id()}_{i}',
                                  is_terminal=False,
                                  node_labels=[v.label() for v in ext])
@@ -243,14 +378,14 @@ def factorize_rule(rule):
         
         # rhs
         rhs = fggs.FactorGraph()
-        for v in bag.nodes:
+        for v in bag:
             rhs.add_node(v)
         rhs.set_ext(ext)
 
         # terminal edges
         for e in rule.rhs().edges():
-            if (bag.nodes.issuperset(e.nodes()) and
-                (parent is None or not parent.nodes.issuperset(e.nodes()))):
+            if (bag.issuperset(e.nodes()) and
+                (parent is None or not parent.issuperset(e.nodes()))):
                 rhs.add_edge(e)
 
         # nonterminal edges and recurse on children
@@ -267,13 +402,13 @@ def factorize_rule(rule):
     
     return newrules
 
-def factorize(g):
+def factorize(g, method='min_fill'):
     """Factorize a FGG's rules into smaller rules, hopefully with
     lower maximum treewidth.
     """
     gnew = fggs.FGGRepresentation()
     for r in g.all_rules():
-        for rnew in factorize_rule(r):
+        for rnew in factorize_rule(r, method=method):
             gnew.add_rule(rnew)
     gnew.set_start_symbol(g.start_symbol())
     return gnew
