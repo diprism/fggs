63adc2483c6dbd1d787500ef07e6898207fa6d0f
David Chiang <dchiang@nd.edu>
2021-07-17 16:55:56 -0400

    let both factorize.py and sum_produt.py select method on command line

--- bin/factorize.py
+++ bin/factorize.py
@@ -10,11 +10,12 @@ if __name__ == '__main__':
     ap = argparse.ArgumentParser(description='Compute the sum-product of an FGG.')
     ap.add_argument('input', metavar='json')
     ap.add_argument('-o', dest='output', metavar='json', required=True)
+    ap.add_argument('-m', dest='method', metavar='method', default='min_fill', choices=['min_fill', 'quickbb', 'acb'])
 
     args = ap.parse_args()
 
     fgg = json_to_fgg(json.load(open(args.input)))
-    fgg = factorize(fgg)
+    fgg = factorize(fgg, method=args.method)
     with open(args.output, 'w') as outfile:
         json.dump(fgg_to_json(fgg), outfile, indent=4)
         
--- bin/sum_product.py
+++ bin/sum_product.py
@@ -9,9 +9,10 @@ from fggs import sum_product, json_to_fgg
 if __name__ == '__main__':
     ap = argparse.ArgumentParser(description='Compute the sum-product of an FGG.')
     ap.add_argument('fgg', metavar='json')
+    ap.add_argument('-m', metavar='method', dest='method', default='fixed-point', choices=['fixed-point', 'broyden'])
 
     args = ap.parse_args()
 
     fgg = json_to_fgg(json.load(open(args.fgg)))
     
-    print(sum_product(fgg))
+    print(sum_product(fgg, method=args.method))
