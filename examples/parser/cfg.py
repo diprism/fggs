c82a86b9aeb263d0cf20008db8c93a29698ac0b5
David Chiang <dchiang@nd.edu>
2021-12-21 16:39:51 -0500

    fix bugs

--- examples/parser/cfg.py
+++ examples/parser/cfg.py
@@ -15,10 +15,14 @@ cfgrules = collections.defaultdict(set)
 traintrees = []
 for line in open(args.trainfile):
     tree = trees.Tree.from_str(line)
+    # Because the Treebank has nonterminals and terminals with the same name, we rename apart the nonterminals
+    for node in tree.bottomup():
+        if len(node.children) > 0:
+            node.label = "nt_" + node.label
     traintrees.append(tree)
     for node in tree.bottomup():
-        if len(node.children) == 0: continue
-        cfgrules[node.label].add(tuple(child.label for child in node.children))
+        if len(node.children) > 0:
+            cfgrules[node.label].add(tuple(child.label for child in node.children))
 
 # There's more than one way to represent a CFG as an FGG.
 # Here, each CFG rule becomes its own FGG rule.
@@ -30,15 +34,11 @@ pos = fggs.NodeLabel('pos')
 pos_dom = fggs.FiniteDomain([None])
 interp.add_domain(pos, pos_dom)
 
-# This is only needed to work around a bug in sum_product
-word = fggs.EdgeLabel('word', [pos, pos], is_terminal=True)
-interp.add_factor(word, fggs.CategoricalFactor([pos_dom, pos_dom], [[1.]]))
-
 nts = {
     nt : fggs.EdgeLabel(nt, [pos, pos], is_nonterminal=True)
     for nt in cfgrules
 }
-hrg = fggs.HRG(nts['TOP'])
+hrg = fggs.HRG(nts['nt_TOP'])
 
 params = {}
 for lhs in cfgrules:
@@ -49,12 +49,10 @@ for lhs in cfgrules:
             newnode = fggs.Node(pos)
             if x in cfgrules: # if x is nonterminal
                 el = nts[x]
-            else:
-                el = word
-            hrhs.add_edge(fggs.Edge(el, [prevnode, newnode]))
+                hrhs.add_edge(fggs.Edge(el, [prevnode, newnode]))
             prevnode = newnode
         el = fggs.EdgeLabel(f'{lhs} -> {" ".join(rhs)}', [], is_terminal=True)
-        params[el] = torch.tensor(-10., requires_grad=True)
+        params[el] = torch.tensor(-3., requires_grad=True)
         interp.add_factor(el, fggs.CategoricalFactor([], 0.)) # will set weight later
         hrhs.add_edge(fggs.Edge(el, []))
         hrhs.ext = [firstnode, prevnode]
@@ -64,9 +62,9 @@ for lhs in cfgrules:
 hrg = fggs.factorize(hrg)
 fgg = fggs.FGG(hrg, interp)
 
-opt = torch.optim.SGD(params.values(), lr=1e-3)
+opt = torch.optim.SGD(params.values(), lr=1e-2)
 
-for epoch in range(10):
+for epoch in range(100):
     train_loss = 0.
     for tree in traintrees:
         for el, fac in interp.factors.items():
@@ -81,12 +79,14 @@ for epoch in range(10):
                 el = fggs.EdgeLabel(f'{lhs} -> {" ".join(rhs)}', [], is_terminal=True)
                 w += params[el]
 
-        z = fggs.sum_product(fgg, method='fixed-point', kmax=5)
+        z = fggs.sum_product(fgg, method='fixed-point')
+
         loss = -w + torch.log(z)
         train_loss += loss.item()
+        print(f'p(tree) = {w}, Z = {z.item()}')
 
         opt.zero_grad()
         loss.backward()
         opt.step()
-    print(f'epoch={epoch+1} train_loss={train_loss}')
+    print(f'epoch={epoch+1} train_loss={train_loss}, Z={z.item()}')
 
