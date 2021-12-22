5557364442f04140f0a5875eb609278e806f0e36
David Chiang <dchiang@nd.edu>
2021-12-22 15:16:58 -0500

    make less dependent on string operations

--- examples/parser/cfg.py
+++ examples/parser/cfg.py
@@ -8,82 +8,80 @@ ap = argparse.ArgumentParser()
 ap.add_argument('trainfile')
 args = ap.parse_args()
 
-# Extract CFG rules from trees. We don't do any binarization or removal of unary rules.
+# Read in training data
+print('read training data')
+traintrees = [trees.Tree.from_str(line) for line in open(args.trainfile)]
 
+# Extract CFG rules from trees. We don't do any binarization or removal of unary rules.
 print('extract CFG')
-cfgrules = collections.defaultdict(set)
-traintrees = []
-for line in open(args.trainfile):
-    tree = trees.Tree.from_str(line)
-    # Because the Treebank has nonterminals and terminals with the same name, we rename apart the nonterminals
+cfg = collections.defaultdict(set)
+nts = {}
+pos = fggs.NodeLabel('pos')
+for tree in traintrees:
+    # Because the Treebank has nonterminals and terminals with the same name, we change the nonterminals to EdgeLabels
     for node in tree.bottomup():
         if len(node.children) > 0:
-            node.label = "nt_" + node.label
-    traintrees.append(tree)
+            if node.label not in nts:
+                nts[node.label] = fggs.EdgeLabel(node.label, [pos, pos], is_nonterminal=True)
+            node.label = nts[node.label]
     for node in tree.bottomup():
         if len(node.children) > 0:
-            cfgrules[node.label].add(tuple(child.label for child in node.children))
+            cfg[node.label].add(tuple(child.label for child in node.children))
 
 # There's more than one way to represent a CFG as an FGG.
 # Here, each CFG rule becomes its own FGG rule.
 
 print('convert to FGG')
+
 interp = fggs.Interpretation()
 
-pos = fggs.NodeLabel('pos')
 pos_dom = fggs.FiniteDomain([None])
 interp.add_domain(pos, pos_dom)
 
-nts = {
-    nt : fggs.EdgeLabel(nt, [pos, pos], is_nonterminal=True)
-    for nt in cfgrules
-}
-hrg = fggs.HRG(nts['nt_TOP'])
+hrg = fggs.HRG(nts['TOP'])
 
+rules = {}
 params = {}
-for lhs in cfgrules:
-    for rhs in cfgrules[lhs]:
+for lhs in cfg:
+    for rhs in cfg[lhs]:
         hrhs = fggs.Graph()
         firstnode = prevnode = fggs.Node(pos)
         for x in rhs:
             newnode = fggs.Node(pos)
-            if x in cfgrules: # if x is nonterminal
-                el = nts[x]
-                hrhs.add_edge(fggs.Edge(el, [prevnode, newnode]))
+            if x in cfg: # if x is nonterminal
+                hrhs.add_edge(fggs.Edge(x, [prevnode, newnode]))
             prevnode = newnode
-        el = fggs.EdgeLabel(f'{lhs} -> {" ".join(rhs)}', [], is_terminal=True)
+        el = fggs.EdgeLabel(f'{lhs} -> {" ".join(map(str,rhs))}', [], is_terminal=True)
+        rules[lhs, rhs] = el
         params[el] = torch.tensor(-5., requires_grad=True)
         interp.add_factor(el, fggs.CategoricalFactor([], 0.)) # will set weight later
         hrhs.add_edge(fggs.Edge(el, []))
         hrhs.ext = [firstnode, prevnode]
-        hrule = fggs.HRGRule(nts[lhs], hrhs)
+        hrule = fggs.HRGRule(lhs, hrhs)
         hrg.add_rule(hrule)
 
 hrg = fggs.factorize(hrg)
 fgg = fggs.FGG(hrg, interp)
 
-opt = torch.optim.SGD(params.values(), lr=1e-3)
+opt = torch.optim.SGD(params.values(), lr=1e-2)
 
 for epoch in range(100):
     train_loss = 0.
     for tree in traintrees:
-        for el, fac in interp.factors.items():
-            if el in params:
-                fac._weights = torch.exp(params[el])
+        for el in params:
+            interp.factors[el]._weights = torch.exp(params[el])
 
         w = 0.
         for node in tree.bottomup():
             if len(node.children) > 0:
                 lhs = node.label
-                rhs = [child.label for child in node.children]
-                el = fggs.EdgeLabel(f'{lhs} -> {" ".join(rhs)}', [], is_terminal=True)
-                w += params[el]
+                rhs = tuple(child.label for child in node.children)
+                w += params[rules[lhs, rhs]]
 
         z = fggs.sum_product(fgg, method='fixed-point', kmax=10, tol=1e-20)
 
         loss = -w + torch.log(z)
         train_loss += loss.item()
-        #print(f'p(tree) = {w}, Z = {z.log().item()}')
 
         opt.zero_grad()
         loss.backward()
