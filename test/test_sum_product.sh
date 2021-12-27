63579542c6f4f5373b8f2109f8eb3fe68397509b
David Chiang <davidweichiang@users.noreply.github.com>
2021-12-27 14:08:16 -0500

    Update sum_product.sh and add test (#95)
    * update sum_product.sh and add test
    * sum_product command-line tool creates factor if necessary
    * add new test to CI

---
+++ test/test_sum_product.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export PYTHONPATH=.:$PYTHONPATH
+
+echo -n "simplefgg.json: "
+
+ANS=`python bin/sum_product.py test/simplefgg.json -w fac1 0.2 -w fac2 0.8`
+PASS=`echo "($ANS - 0.25) ^ 2 < 0.0000001" | bc`
+if [ $PASS -eq 1 ]; then
+    echo "passed"
+else
+    echo "failed"
+    exit 1
+fi
