14d3b3e76521a3408f95746ed956a885fab83024
David Chiang <dchiang@nd.edu>
2021-12-26 21:15:58 -0500

    add tests for reading/writing Interpretations

--- test/test_formats.py
+++ test/test_formats.py
@@ -22,23 +22,28 @@ class TestJSON(unittest.TestCase):
         for filename in ['hmm.json', 'example12p.json', 'simplefgg.json']:
             with open(os.path.join(os.path.dirname(__file__), filename)) as f:
                 j = json.load(f)
-            g = fggs.json_to_fgg(j)
-            j_check = fggs.fgg_to_json(g)
+            fgg = fggs.json_to_fgg(j)
+            j_check = fggs.fgg_to_json(fgg)
 
-            j = j['grammar']
-            j_check = j_check['grammar']
+            jg = j['grammar']
+            jg_check = j_check['grammar']
 
             self.maxDiff = 10000
-            self.assertEqual(j.keys(), j_check.keys())
-            self.assertEqual(j['terminals'], j_check['terminals'])
-            self.assertEqual(j['nonterminals'], j_check['nonterminals'])
-            self.assertEqual(j['start'], j_check['start'])
+            self.assertEqual(jg.keys(), jg_check.keys())
+            self.assertEqual(jg['terminals'], jg_check['terminals'])
+            self.assertEqual(jg['nonterminals'], jg_check['nonterminals'])
+            self.assertEqual(jg['start'], jg_check['start'])
 
             # ignore order of rules
-            for r in j['rules']:
-                self.assertTrue(r in j_check['rules'], f'rule: {r}\nnot found in: {j_check["rules"]}')
-            for r in j_check['rules']:
-                self.assertTrue(r in j['rules'], r)
+            for r in jg['rules']:
+                self.assertTrue(r in jg_check['rules'], f'rule: {r}\nnot found in: {jg_check["rules"]}')
+            for r in jg_check['rules']:
+                self.assertTrue(r in jg['rules'], r)
+
+            ji = j['interpretation']
+            ji_check = j['interpretation']
+            self.assertEqual(ji['domains'], ji_check['domains'])
+            self.assertEqual(ji['factors'], ji_check['factors'])
 
     def test_error(self):
         with open(os.path.join(os.path.dirname(__file__), 'hmm.json')) as f:
