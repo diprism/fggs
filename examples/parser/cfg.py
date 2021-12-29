12a7f6d720435a439194e03527f3d704833c442a
David Chiang <dchiang@nd.edu>
2021-12-29 15:35:41 -0500

    minibatching

--- examples/parser/cfg.py
+++ examples/parser/cfg.py
@@ -3,6 +3,7 @@ import fggs
 import trees
 import argparse
 import collections
+import tqdm
 
 ap = argparse.ArgumentParser()
 ap.add_argument('trainfile')
@@ -65,28 +66,45 @@ fgg = fggs.FGG(hrg, interp)
 
 opt = torch.optim.SGD(params.values(), lr=1e-2)
 
+def minibatches(iterable, size):
+    b = []
+    for i, x in enumerate(iterable):
+        if i % size == 0 and len(b) > 0:
+            yield b
+            b = []
+        b.append(x)
+    if len(b) > 0:
+        yield b
+
+minibatch_size = 100
+
 for epoch in range(100):
     train_loss = 0.
-    for tree in traintrees:
-        for el in params:
-            interp.factors[el].weights = torch.exp(params[el])
+    train_sents = 0
+    with tqdm.tqdm(total=len(traintrees)) as progress:
+        for minibatch in minibatches(traintrees, minibatch_size):
+            for el in params:
+                interp.factors[el].weights = torch.exp(params[el])
+            w = 0.
+            for tree in minibatch:
+                for node in tree.bottomup():
+                    if len(node.children) > 0:
+                        lhs = node.label
+                        rhs = tuple(child.label for child in node.children)
+                        w += params[rules[lhs, rhs]]
 
-        w = 0.
-        for node in tree.bottomup():
-            if len(node.children) > 0:
-                lhs = node.label
-                rhs = tuple(child.label for child in node.children)
-                w += params[rules[lhs, rhs]]
+            # Newton's method currently works better than fixed-point iteration
+            # for avoiding z = 0.
+            z = fggs.sum_product(fgg, method='newton', kmax=100, tol=1e-30)
 
-        # Newton's method currently works better than fixed-point iteration
-        # for avoiding z = 0.
-        z = fggs.sum_product(fgg, method='newton', kmax=100, tol=1e-30)
+            loss = -w + minibatch_size * torch.log(z)
+            train_loss += loss.item()
 
-        loss = -w + torch.log(z)
-        train_loss += loss.item()
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
 
-        opt.zero_grad()
-        loss.backward()
-        opt.step()
-    print(f'epoch={epoch+1} train_loss={train_loss}, Z={z.log().item()}')
+            train_sents += len(minibatch)
+            progress.update(train_sents)
+    print(f'epoch={epoch+1} train_loss={train_loss}')
 
