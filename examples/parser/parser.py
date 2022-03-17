910e589d65c3c727297e2cda7d1071cae3b6f9e1
David Chiang <dchiang@nd.edu>
2022-03-17 11:11:23 -0400

    a little reorganization

--- examples/parser/parser.py
+++ examples/parser/parser.py
@@ -48,29 +48,31 @@ for tree in traintrees:
 print('convert to FGG')
 
 interp = fggs.Interpretation()
-params = {}
 
 if args.method == 'rule':
-    rules = {}
-    
     hrg = fggs.HRG('TOP')
-    fgg = fggs.FGG(hrg, interp)
-
+    
+    rules = {}
     for lhs in cfg:
         for rhs in cfg[lhs]:
-            el = f'{repr(lhs)} -> {" ".join(map(repr, rhs))}'
-            rules[lhs, rhs] = el
-            
             hrhs = fggs.Graph()
+            
+            # One nonterminal edge for each CFG nonterminal
             for x in rhs:
                 if isinstance(x, Nonterminal):
                     hrhs.new_edge(x, [], is_nonterminal=True)
+                    
+            # One terminal edge for each rule
+            el = f'{repr(lhs)} -> {" ".join(map(repr, rhs))}'
+            rules[lhs, rhs] = el # save for later use
             hrhs.new_edge(el, [], is_terminal=True)
+            
             hrhs.ext = []
             hrg.new_rule(lhs, hrhs)
-            
-            params[el] = torch.tensor(0., requires_grad=True)
-            fgg.new_categorical_factor(el, 0.) # will set weight later
+
+    fgg = fggs.FGG(hrg, interp)
+    for el in rules.values():
+        fgg.new_categorical_factor(el, torch.tensor(0., requires_grad=True))
 
 elif args.method == 'pattern':
     for lhs in cfg:
@@ -88,23 +90,18 @@ elif args.method == 'pattern':
                         terminals.add(x)
                         
     hrg = fggs.HRG('tree')
-    fgg = fggs.FGG(hrg, interp)
     
-    nonterminal_dom = fgg.new_finite_domain('nonterminal', nonterminals)
-    terminal_dom = fgg.new_finite_domain('terminal', terminals)
-    
-    pattern_els = {}
-
     hrhs = fggs.Graph()
     root = hrhs.new_node('nonterminal')
     hrhs.new_edge('is_start', [root], is_terminal=True)
     hrhs.new_edge('subtree', [root], is_nonterminal=True)
     hrg.new_rule('tree', hrhs)
-    weights = torch.tensor([x == 'TOP' for x in nonterminals], dtype=torch.get_default_dtype())
-    fgg.new_categorical_factor('is_start', weights)
-
+    
+    pattern_els = {}
     for pattern in patterns:
         hrhs = fggs.Graph()
+
+        # A node for each child, and a nonterminal edge for each CFG nonterminal
         parent = hrhs.new_node('nonterminal')
         children = []
         for is_nonterminal in pattern:
@@ -114,16 +111,27 @@ elif args.method == 'pattern':
             else:
                 child = hrhs.new_node('terminal')
             children.append(child)
+            
+        # One terminal edge for each rule
         el = ' '.join(child.label.name for child in children)
         pattern_els[pattern] = el
         hrhs.new_edge(el, [parent]+children, is_terminal=True)
+        
         hrhs.ext = [parent]
         hrg.new_rule('subtree', hrhs)
-        
+
+    fgg = fggs.FGG(hrg, interp)
+    
+    nonterminal_dom = fgg.new_finite_domain('nonterminal', nonterminals)
+    terminal_dom = fgg.new_finite_domain('terminal', terminals)
+    
+    fgg.new_categorical_factor(
+        'is_start',
+        torch.tensor([float(x == 'TOP') for x in nonterminals]))
+
+    for pattern, el in pattern_els.items():
         shape = (len(nonterminals),) + tuple(len(nonterminals) if p else len(terminals) for p in pattern)
-        params[el] = torch.zeros(shape, requires_grad=True)
-        weights = torch.zeros(shape) # will set weights later
-        fgg.new_categorical_factor(el, weights)
+        fgg.new_categorical_factor(el, torch.zeros(shape, requires_grad=True))
 
 else:
     print(f'unknown method: {args.method}', file=sys.stderr)
@@ -134,7 +142,8 @@ fgg = fggs.FGG(hrg, interp)
 
 print('begin training')
 # The learning rate should be set low enough that we don't easily jump out of the region where Z is finite.
-opt = torch.optim.SGD(params.values(), lr=1e-3)
+params = [fac.weights for fac in interp.factors.values() if fac.weights.requires_grad]
+opt = torch.optim.SGD(params, lr=1e-3)
 
 def minibatches(iterable, size):
     b = []
@@ -159,17 +168,14 @@ for epoch in range(100):
                         lhs = node.label
                         rhs = tuple(child.label for child in node.children)
                         if args.method == 'rule':
-                            w += params[rules[lhs, rhs]]
+                            w += interp.factors[hrg.get_edge_label(rules[lhs, rhs])].weights
                         elif args.method == 'pattern':
                             pattern = tuple(isinstance(x, Nonterminal) for x in rhs)
                             lhs_index = nonterminal_dom.numberize(lhs)
                             rhs_indices = tuple(nonterminal_dom.numberize(x) if isinstance(x, Nonterminal) else terminal_dom.numberize(x) for x in rhs)
-                            w += params[pattern_els[pattern]][(lhs_index,)+rhs_indices]
+                            w += interp.factors[hrg.get_edge_label(pattern_els[pattern])].weights[(lhs_index,)+rhs_indices]
                         else:
                             assert False
-
-            for el in params:
-                interp.factors[fgg.grammar.get_edge_label(el)].weights = params[el]
                 
             z = fggs.sum_product(fgg, method='fixed-point', semiring=fggs.LogSemiring())
 
@@ -181,7 +187,7 @@ for epoch in range(100):
             # Gradient clipping is crucial, since the gradient can have infinite components.
             # The clipping value should be high enough to quickly exit the region where Z is infinite.
             # The reciprocal of the learning rate seems to be a reasonable choice.
-            torch.nn.utils.clip_grad_value_(params.values(), 1000.)
+            torch.nn.utils.clip_grad_value_(params, 1000.)
             opt.step()
 
             progress.update(len(minibatch))
