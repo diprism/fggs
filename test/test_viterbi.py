5f0d106a5ca7524e8e53ebb3e4b7692ed6887fe3
David Chiang <dchiang@nd.edu>
2022-03-21 13:53:21 -0400

    Add Viterbi algorithm that can reconstruct factor graphs and assignments

---
+++ test/test_viterbi.py
@@ -0,0 +1,52 @@
+from fggs import FGG, Graph, ViterbiSemiring, json_to_fgg
+from fggs.viterbi import viterbi
+from fggs.utils import naive_graph_isomorphism
+import unittest, torch, json, copy
+
+def load_fgg(filename):
+    with open(filename) as f:
+        return json_to_fgg(json.load(f))
+
+class TestSumProduct(unittest.TestCase):
+
+    def setUp(self):
+        self.fgg = load_fgg('test/hmm.json')
+        self.graph = g = Graph()
+        bos = g.new_node('T')
+        dt  = g.new_node('T')
+        the = g.new_node('W')
+        nn  = g.new_node('T')
+        cat = g.new_node('W')
+        eos = g.new_node('T')
+        g.new_edge('is_bos',     [bos],      is_terminal=True)
+        g.new_edge('transition', [bos, dt],  is_terminal=True)
+        g.new_edge('transition', [dt,  nn],  is_terminal=True)
+        g.new_edge('transition', [nn,  eos], is_terminal=True)
+        g.new_edge('emission',   [dt,  the], is_terminal=True)
+        g.new_edge('emission',   [nn,  cat], is_terminal=True)
+        g.new_edge('is_eos',     [eos],      is_terminal=True)
+
+        tdom = self.fgg.interp.domains[self.fgg.grammar.get_node_label('T')]
+        wdom = self.fgg.interp.domains[self.fgg.grammar.get_node_label('W')]
+        
+        self.asst = a = {}
+        a[bos] = tdom.numberize('BOS')
+        a[dt]  = tdom.numberize('DT')
+        a[the] = wdom.numberize('the')
+        a[nn]  = tdom.numberize('NN')
+        a[cat] = wdom.numberize('cat')
+        a[eos] = tdom.numberize('EOS')
+        
+    def test_viterbi(self):
+        interp = copy.deepcopy(self.fgg.interp)
+        for fac in interp.factors.values():
+            fac.weights = torch.log(torch.as_tensor(fac.weights))
+        fgg = FGG(self.fgg.grammar, interp)
+        (facgraph, asst) = viterbi(fgg, (), {'semiring': ViterbiSemiring()})
+        result, m = naive_graph_isomorphism(facgraph.graph, self.graph)
+        self.assertTrue(result, m)
+        for node in asst:
+            self.assertEqual(asst[node], self.asst[m[node]])
+                
+if __name__ == '__main__':
+    unittest.main()
