5c3d034b7f2610d7fe613954baf4780eca544e39
David Chiang <dchiang@nd.edu>
2022-03-22 16:25:41 -0400

    add test for infinite gradient; remove one more nan_to_num and document the last remaining one outside of semirings.py

--- fggs/sum_product.py
+++ fggs/sum_product.py
@@ -18,10 +18,6 @@ def _formatwarning(message, category, filename=None, lineno=None, file=None, lin
     return '%s:%s: %s: %s' % (filename, lineno, category.__name__, message)
 warnings.formatwarning = _formatwarning # type: ignore
 
-def tensordot(a, b, n):
-    # https://github.com/pytorch/pytorch/issues/61096 (PyTorch 1.9.0)
-    return torch.tensordot(a, b, n) if n > 0 \
-        else a.reshape(a.size() + (1,) * b.dim()) * b
 
 def scc(g: HRG) -> List[HRG]:
     """Decompose an HRG into a its strongly-connected components using Tarjan's algorithm.
@@ -150,6 +146,10 @@ def J(fgg: FGG, x: MultiTensor, inputs: MultiTensor, semiring: Semiring,
     return Jx
 
 
+def log_softmax(a: Tensor, dim: int) -> Tensor:
+    # a could have infinite elements, which would make log_softmax return all nans.
+    return torch.log_softmax(a.nan_to_num(), dim)
+    
 def J_log(fgg: FGG, x: MultiTensor, inputs: MultiTensor, semiring: Semiring,
           J_terminals: Optional[MultiTensor] = None) -> MultiTensor:
     """The Jacobian of F(semiring=LogSemiring), computed in the real semiring."""
@@ -163,7 +163,7 @@ def J_log(fgg: FGG, x: MultiTensor, inputs: MultiTensor, semiring: Semiring,
             tau_rule = sum_product_edges(interp, rule.rhs.nodes(), rule.rhs.edges(), rule.rhs.ext, x, inputs, semiring=semiring)
             tau_rules.append(tau_rule)
         tau_rules = torch.stack(tau_rules, dim=0)
-        tau_rules = torch.log_softmax(tau_rules, dim=0).nan_to_num()
+        tau_rules = log_softmax(tau_rules, dim=0)
         for rule, tau_rule in zip(rules, tau_rules):
             for edge in rule.rhs.edges():
                 if edge.label.is_terminal and J_terminals is None: continue
@@ -171,7 +171,7 @@ def J_log(fgg: FGG, x: MultiTensor, inputs: MultiTensor, semiring: Semiring,
                 tau_edge = sum_product_edges(interp, rule.rhs.nodes(), rule.rhs.edges(), ext, x, inputs, semiring=semiring)
                 tau_edge_size = tau_edge.size()
                 tau_edge = tau_edge.reshape(tau_rule.size() + (-1,))
-                tau_edge = torch.log_softmax(tau_edge, dim=-1).nan_to_num()
+                tau_edge = log_softmax(tau_edge, dim=-1)
                 tau_edge += tau_rule.unsqueeze(-1)
                 tau_edge = tau_edge.reshape(tau_edge_size)
                 if edge.label.is_terminal:
